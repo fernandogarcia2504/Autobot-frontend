eba087fe53397c912865573836d42fc122117d7e
fernando2504 <nando.garcia2504@gmail.com>
2024-05-07 21:09:24 -0600

    bug fixed 2

--- autobot/src/app/page.tsx
+++ autobot/src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <>
     <Navbar />
-    <main className="flex min-h-screen flex-col m-15  items-center ">
-      <div className="grid grid-cols-first-block mt-12 w-1242">
+    <main className="flex min-h-full flex-col m-15 items-center ">
+      <div className="grid grid-cols-first-block pt-12 w-1242">
         <div className="flex flex-col items-center justify-center gap-3 w-687">
           <h1 className="font-bold text-7xl  ">Optimiza tu flujo de trabajo y potencia tu negocio con <span className="text-green">AutoBot</span>.</h1>
           <p className="font-bold text-3xl">Al automatizar tareas  y procesos repetitivos, AutoBot te ayuda a ahorrar tiempo, impulsando tu rentabilidad y productividad.</p>
